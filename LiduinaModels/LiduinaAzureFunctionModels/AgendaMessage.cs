68afd5312773c98dbc1034486795bab305664aee
Sandra van Dijk <sandra.vandijk@varovafashion.nl>
2017-03-26 18:14:05 +0200

    One working AgendaFunction setup, with model in different solution.

---
+++ LiduinaModels/LiduinaAzureFunctionModels/AgendaMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace LiduinaAzureFunctionModels
+{
+    public class AgendaMessage
+    {
+        public AgendaMessage(string rowKey)
+        {
+            RowKey = rowKey;
+        }
+
+        public AgendaMessage() { }
+
+        public string RowKey { get; set; }
+    }
+}
---
+++ LiduinaModels/LiduinaAzureFunctionModels/OauthKey.cs
@@ -0,0 +1,17 @@
+﻿namespace LiduinaAzureFunctionModels
+{
+    public class OauthKey
+    {
+        public OauthKey(string userId, string agendaProvider)
+        {
+            RowKey = userId;
+            PartitionKey = agendaProvider;
+        }
+
+        public OauthKey() { }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Key { get; set; }
+    }
+}
