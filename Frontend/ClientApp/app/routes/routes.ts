322204cfeb6807b1110a4fba8c3e10df99d782cf
Sandra van Dijk <sandra.vandijk@varovafashion.nl>
2017-04-18 22:03:45 +0200

    Work in progess: functions with (temp) frontend to get oauth token.

---
+++ Frontend/ClientApp/app/routes/routes.ts
@@ -0,0 +1,11 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { AuthManager } from './authmanager';
+
+export const appRoutes: Routes = [
+	{ path: '', component: LoginComponent },
+	{ path: 'login', component: LoginComponent, canActivate: [AuthManager] }
+];
+
+export const AppRouterProvider = RouterModule.forRoot(appRoutes);
--- Functions/Liduina.UserFunctions/Properties/AssemblyInfo.cs
+++ Functions/Liduina.UserFunctions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgendaFunctions")]
+[assembly: AssemblyTitle("Liduina.UserFunctions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgendaFunctions")]
+[assembly: AssemblyProduct("Liduina.UserFunctions")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db97be35-fd78-4a39-b66a-bb44e48173e6")]
+[assembly: Guid("e77bc454-f0cb-43a2-8132-fe7303721cee")]
 
 // Version information for an assembly consists of the following four values:
 //
