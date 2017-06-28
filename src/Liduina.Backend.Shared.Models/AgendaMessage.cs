1b54493e7cb91fbaeb13a15c768dc0d720939311
Christiaan Molendijk <c.molendijk@chrismo.nl>
2017-06-28 22:50:25 +0200

    Created the Agenda function in new solution and with new function tools.

---
+++ src/Liduina.Backend.Shared.Models/AgendaMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Liduina.Backend.Shared.Models
+{
+    public class AgendaMessage
+    {
+        public AgendaMessage(string rowKey)
+        {
+            RowKey = rowKey;
+        }
+
+        public AgendaMessage() { }
+
+        public string RowKey { get; set; }
+    }
+}
