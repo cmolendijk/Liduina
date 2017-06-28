367790497ad5e4dc5866b2ecd536d22a05830bb6
Christiaan Molendijk <c.molendijk@chrismo.nl>
2017-06-28 22:59:33 +0200

    Getting getting data from table.

--- src/Liduina.Backend.Workers.AgendaFunctions/Outlook.cs
+++ src/Liduina.Backend.Workers.AgendaFunctions/Outlook.cs
@@ -8,9 +8,12 @@ namespace Liduina.Backend.Workers.AgendaFunctions
     public static class Outlook
     {
         [FunctionName("agendaMessage")]
-        public static void Run([ServiceBusTrigger("agendatopic", "Outlook", AccessRights.Manage, Connection = "AzureWebJobsServiceBus")]AgendaMessage agendaMessage, TraceWriter log)
+        public static void Run(
+            [ServiceBusTrigger("agendatopic", "Outlook", AccessRights.Manage, Connection = "AzureWebJobsServiceBus")]AgendaMessage agendaMessage, 
+            [Table("oauthKey", "Outlook", "{RowKey}", Connection = "AzureWebJobsStorage")] OauthKey oauthKey,
+            TraceWriter log)
         {
-            log.Info($"C# ServiceBus topic trigger function processed message with row key: {agendaMessage.RowKey}");
+            log.Info($"C# ServiceBus topic trigger function processed message with row key: {agendaMessage.RowKey} and key: {oauthKey.Key}");
         }
     }
 }
